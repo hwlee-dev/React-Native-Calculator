d51aca7520a10e4001f44a455ecde239342b69aa
Hyunwoong Lee <55308484+hwlee-dev@users.noreply.github.com>
2020-12-09 22:58:33 +0900

    add calculator display area
    - add calculator display area
    - provide expression and result

---
+++ src/components/molecules/Display/Display.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  NativeSyntheticEvent,
+  TextInputSelectionChangeEventData,
+} from 'react-native';
+import styles from './styles';
+
+interface displayProps {
+  expression: string;
+  result: string;
+  style: any;
+  handleSelectionChange: (
+    event: NativeSyntheticEvent<TextInputSelectionChangeEventData>,
+  ) => void;
+}
+
+/**
+ * 계산기 표현부
+ */
+const Display: React.FC<displayProps> = (props: displayProps) => {
+  return (
+    <View style={[styles.container, props.style?.container]}>
+      <TextInput
+        // onChangeText={(text) => onChangeText(text)}
+        value={props.expression}
+        style={[styles.input, props.style?.input]}
+        onSelectionChange={(event) => props.handleSelectionChange(event)}
+        multiline={true}
+      />
+      <Text style={[styles.result, props.style?.result]}>{11111111111}</Text>
+    </View>
+  );
+};
+
+export default Display;
---
+++ src/components/molecules/Display/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {},
+  input: {
+    margin: 5,
+    textAlign: 'right',
+  },
+  result: {
+    margin: 5,
+    textAlign: 'right',
+  },
+});
