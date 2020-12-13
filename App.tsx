8817d6a4243df054ab2f748142c802db4e3922e8
Hyunwoong Lee <55308484+hwlee-dev@users.noreply.github.com>
2020-12-13 23:35:40 +0900

    app changed
    - add statusbar
    - add styles

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Main from './src/pages/Calculator';
+import { StatusBar, View } from 'react-native';
+import Main from './src/pages/Main';
+import styles from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.mainContainer}>
+      <StatusBar barStyle='light-content' />
       <Main />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'flex-end' },
-});
