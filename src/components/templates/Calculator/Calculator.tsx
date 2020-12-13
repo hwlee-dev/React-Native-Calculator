cfc86d05a5efc7ad4c8684e9d6e84f1d6f137d47
Hyunwoong Lee <55308484+hwlee-dev@users.noreply.github.com>
2020-12-13 23:31:48 +0900

    calculator template changed
    - change state values
    - change styles

--- src/components/templates/Calculator/Calculator.tsx
+++ src/components/templates/Calculator/Calculator.tsx
@@ -1,67 +1,34 @@
 import React, { useState } from 'react';
-import {
-  View,
-  NativeSyntheticEvent,
-  TextInputSelectionChangeEventData,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
+import { View } from 'react-native';
 import Display from '../../molecules/Display';
 import Keypad from '../../molecules/Keypad';
-import calculator from '../../../services/calculator';
+import calculator from '../../../utils/calculator';
 import styles from './styles';
 
-const Calculator: React.FC = () => {
-  const [displayValues, setDisplayValues] = useState({
-    expression: '',
-    result: '',
-    cursorPosition: 0,
+const Calculator = () => {
+  const [values, setValues] = useState({
+    displayValue: '0',
+    previousValue: '',
+    currentValue: '0',
+    operator: '',
   });
 
-  const handlePress = (type: string, value = '') => {
-    const result = calculator(
-      type,
-      value,
-      displayValues.expression,
-      displayValues.cursorPosition,
-    );
-
-    setDisplayValues({
-      ...displayValues,
-      ['expression']: result?.expression,
-      ['cursorPosition']: result?.cursorPosition,
+  const handlePress = (newType: string, newValue: string) => {
+    const result = calculator({
+      type: newType,
+      value: newValue,
+      ...values,
     });
-    // console.log(`calculator.tsx : ${type} ${value} ${displayValues.expression}`);
-  };
 
-  const handleSelectionChange = (
-    event: NativeSyntheticEvent<TextInputSelectionChangeEventData>,
-  ) => {
-    setDisplayValues({
-      ...displayValues,
-      ['cursorPosition']: event.nativeEvent.selection.start,
+    setValues({
+      ...values,
+      ...result,
     });
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.displayWrapper}>
-        <Display
-          style={{
-            container: styles.displayContainer,
-            input: styles.displayInput,
-            result: styles.displayResult,
-          }}
-          expression={displayValues.expression}
-          result={displayValues.result}
-          handleSelectionChange={handleSelectionChange}
-        />
-      </View>
-      <View style={styles.deleteWrapper}>
-        <TouchableOpacity style={styles.deleteButton}>
-          <Text>DEL</Text>
-        </TouchableOpacity>
-      </View>
+      <Display displayValue={values.displayValue} />
       <View style={styles.divider} />
       <Keypad handlePress={handlePress} />
     </View>
--- src/components/templates/Calculator/styles.ts
+++ src/components/templates/Calculator/styles.ts
@@ -1,38 +1,23 @@
 import { StyleSheet } from 'react-native';
+import { colors, margin } from '../../../styles/common';
 
 export default StyleSheet.create({
-  container: { flex: 1, alignItems: 'stretch' },
-  displayWrapper: { flex: 4 },
-  displayContainer: { flex: 1 },
-  displayInput: {
-    flex: 3,
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  displayText: {
+    flex: 1,
     marginTop: 30,
     marginRight: 20,
     marginBottom: 50,
     fontSize: 40,
   },
-  displayResult: {
-    flex: 1,
-    marginRight: 20,
-    marginBottom: 30,
-    justifyContent: 'center',
-    fontSize: 25,
-  },
-  deleteWrapper: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  deleteButton: {
-    width: 30,
-    height: 25,
-    marginRight: 30,
-  },
   divider: {
     width: '92%',
     alignSelf: 'center',
-    borderBottomColor: '#E1E1E1',
+    borderBottomColor: colors.divider,
     borderBottomWidth: 2,
+    marginVertical: margin.md,
   },
 });
