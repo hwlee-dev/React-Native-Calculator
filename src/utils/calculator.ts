d095007bfc64499f20c0f1d851c7effae0402dd3
Hyunwoong Lee <55308484+hwlee-dev@users.noreply.github.com>
2020-12-15 00:34:16 +0900

    calculator utils changed
    - declare constant variables
    - changed calculator logic

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -6,16 +6,20 @@ interface paramType {
   currentValue: string;
 }
 
+const MAX_VALUE = 999999999;
+const MAX_LENGTH = 9;
+const MAX_EXPONENTIAL_LENGTH = 5;
+
 /**
  * 계산기 처리
- * @param params => paramType 값
+ * @param params paramType 값
  */
 const calculator = (params: paramType) => {
   const { type } = params;
   const initialValue = {
     displayValue: '0',
     previousValue: '',
-    currentValue: '0',
+    currentValue: '',
     operator: '',
   };
 
@@ -25,21 +29,27 @@ const calculator = (params: paramType) => {
     case 'number':
       result = handleNumber(params);
       break;
+
     case 'operator':
       result = handleOperator(params);
       break;
+
     case 'percentage':
       result = handlePercentage(params);
       break;
+
     case 'posneg':
       result = handlePosNeg(params);
       break;
+
     case 'equal':
       result = handleEqual(params);
       break;
+
     case 'clear':
       result = initialValue;
       break;
+
     default:
       break;
   }
@@ -49,12 +59,13 @@ const calculator = (params: paramType) => {
 
 /**
  * 10자리 이상의 숫자 포맷
- * @param value => 포맷할 값
+ * @param value 포맷할 값
  */
 const formatDisplayValue = (value: string) => {
   const num = parseFloat(value);
-  if (num > 999999999 || value.split('e').length > 1) {
-    return num.toExponential(5).split('+').join('');
+
+  if (num > MAX_VALUE || value.split('e').length > 1) {
+    return num.toExponential(MAX_EXPONENTIAL_LENGTH).split('+').join('');
   } else {
     return value;
   }
@@ -62,12 +73,12 @@ const formatDisplayValue = (value: string) => {
 
 /**
  * 숫자 입력 처리
- * @param params => paramType 값
+ * @param params paramType 값
  */
 const handleNumber = (params: paramType) => {
   const { value, currentValue } = params;
 
-  if (currentValue.split('.').join('').length === 9) {
+  if (currentValue.split('.').join('').length === MAX_LENGTH) {
     return {};
   }
 
@@ -75,8 +86,8 @@ const handleNumber = (params: paramType) => {
     return currentValue.indexOf('.') > -1
       ? {}
       : {
-          currentValue: `${currentValue}${value}`,
-          displayValue: formatDisplayValue(`${currentValue}${value}`),
+          currentValue: `${currentValue || '0'}${value}`,
+          displayValue: formatDisplayValue(`${currentValue || '0'}${value}`),
         };
   }
 
@@ -93,109 +104,141 @@ const handleNumber = (params: paramType) => {
 
 /**
  * 사칙연산 입력 처리
- * @param params => paramType 값
+ * @param params paramType 값
  */
 const handleOperator = (params: paramType) => {
   const { value, previousValue, currentValue, operator } = params;
-  console.log(params);
 
-  if (operator === value && currentValue === '0') {
-    return {};
+  if (!previousValue && currentValue && value) {
+    return {
+      displayValue: '0',
+      previousValue: currentValue,
+      currentValue: '',
+      operator: value,
+    };
   }
 
-  // 이전 값과 현재 값이 있고 사칙연산이 입력된 경우
-  if (previousValue && currentValue) {
+  if (previousValue && currentValue && operator) {
     const result = handleEqual(params);
-    result.currentValue = '0';
-    result.operator = value;
 
-    return result;
+    return {
+      ...result,
+      operator: value,
+    };
   }
 
-  return {
-    displayValue: formatDisplayValue(currentValue),
-    previousValue: currentValue,
-    currentValue: '0',
-    operator: value,
-  };
+  if (previousValue && !currentValue) {
+    return {
+      displayValue: '0',
+      currentValue: '',
+      operator: value,
+    };
+  }
+
+  if (currentValue && !operator) {
+    return {
+      currentValue: '',
+      displayValue: '0',
+      operator: value,
+      previousValue: currentValue,
+    };
+  }
+
+  return {};
 };
 
 /**
  * 퍼센트 입력 처리
- * @param params => paramType 값
+ * @param params paramType 값
  */
 const handlePercentage = (params: paramType) => {
-  const { operator } = params;
-  let percentValue = '';
-
-  if (operator !== '') {
-    const result = handleEqual(params);
-    percentValue = (parseFloat(result.currentValue) * 0.01).toString();
-  } else {
-    percentValue = (parseFloat(params.currentValue) * 0.01).toString();
-  }
+  const { previousValue, currentValue } = params;
+  const percentageValue = currentValue ? currentValue : previousValue;
+  const parsedValue = (parseFloat(percentageValue) * 0.01).toString();
 
   return {
-    displayValue: formatDisplayValue(percentValue),
-    previousValue: '',
-    currentValue: percentValue,
-    operator: '',
+    displayValue: formatDisplayValue(parsedValue),
+    previousValue: !currentValue ? parsedValue : previousValue,
+    currentValue: currentValue ? parsedValue : currentValue,
   };
 };
 
 /**
  * +/- 입력 처리
- * @param params => paramType 값
+ * @param params paramType 값
  */
 const handlePosNeg = (params: paramType) => {
-  const { currentValue } = params;
+  const { previousValue, currentValue } = params;
+  const posnegValue = currentValue ? currentValue : previousValue;
 
-  if (currentValue[0] === '-') {
+  if (!previousValue && !currentValue) {
     return {
-      currentValue: currentValue.substr(1),
-      displayValue: formatDisplayValue(currentValue.substr(1)),
+      displayValue: '-0',
+      previousValue: '',
+      currentValue: '-0',
+    };
+  }
+
+  if (previousValue && !currentValue) {
+    return {
+      displayValue: '-0',
+      currentValue: '-0',
+    };
+  }
+
+  if (posnegValue[0] === '-') {
+    return {
+      currentValue: posnegValue.substr(1),
+      displayValue: formatDisplayValue(posnegValue.substr(1)),
     };
   }
 
   return {
-    currentValue: '-' + currentValue,
-    displayValue: formatDisplayValue('-' + currentValue),
+    currentValue: '-' + (posnegValue || '0'),
+    displayValue: formatDisplayValue('-' + (posnegValue || '0')),
   };
 };
 
 /**
  * 연산 처리
- * @param params => paramType 값
+ * @param params paramType 값
  */
 const handleEqual = (params: paramType) => {
   const { previousValue, currentValue, operator } = params;
   const firstValue = parseFloat(previousValue);
-  const secondValue =
-    currentValue === '0' ? parseFloat(previousValue) : parseFloat(currentValue);
+  let secondValue =
+    currentValue === '' ? parseFloat(previousValue) : parseFloat(currentValue);
   let result = 0;
 
   switch (operator) {
     case '+':
       result = firstValue + secondValue;
       break;
+
     case '-':
       result = firstValue - secondValue;
       break;
+
     case '*':
       result = firstValue * secondValue;
       break;
+
     case '/':
       result = firstValue / secondValue;
+      result = isFinite(result) ? result : 0;
       break;
+
     default:
+      result = secondValue;
+      secondValue = 0;
       break;
   }
 
   return {
     displayValue: formatDisplayValue(result.toString()),
     previousValue: result.toString(),
-    currentValue: secondValue.toString(),
-    operator: operator,
+    currentValue: '',
+    operator: '',
   };
 };
 
